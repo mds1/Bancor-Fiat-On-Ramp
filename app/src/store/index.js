4057b1060f811080bddc4742a1c1d4a86c5afb14
mds <17163988+mds1@users.noreply.github.com>
2020-03-23 16:27:12 -0700

    Fix lint issues

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-// import example from './module-example'
+import main from './main';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 /*
  * If not building with SSR mode, you can
@@ -17,13 +17,13 @@ Vue.use(Vuex)
 export default function (/* { ssrContext } */) {
   const Store = new Vuex.Store({
     modules: {
-      // example
+      main,
     },
 
     // enable strict mode (adds overhead!)
     // for dev mode only
-    strict: process.env.DEV
-  })
+    strict: process.env.DEV,
+  });
 
-  return Store
+  return Store;
 }
